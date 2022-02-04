4fd2c7a33d5508819cd0b9b2f7b251b8c04c272a
marcouellet <marc.ouellet2@gmail.com>
2022-02-04 09:38:05 -0500

    initial

---
+++ sample-app/server.js
@@ -0,0 +1,23 @@
+var http = require("http");
+var os = require("os");
+var hostname = os.hostname();
+
+var qs = require("qs");
+var moment = require("moment");
+
+var port = process.env.PORT || 3000;
+
+function handleRequest(request, response) {
+  response.end(
+    "[" +
+      hostname +
+      "] Serving requests from simple nodejs k8s master. Request URL:" +
+      request.url
+  );
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(port, function () {
+  console.log("My Server is listening on port", port);
+});
