6761c5a7962810c31e793368bf96475fd990957f
marcouellet <marc.ouellet2@gmail.com>
2022-02-06 17:33:08 -0500

    ***development***

--- sample-app/server.js
+++ sample-app/server.js
@@ -11,7 +11,7 @@ function handleRequest(request, response) {
   response.end(
     "[" +
       hostname +
-      "] Serving requests from simple nodejs k8s master ***dev***. Request URL:" +
+      "] Serving requests from simple nodejs k8s master *** development ***. Request URL:" +
       request.url
   );
 }
